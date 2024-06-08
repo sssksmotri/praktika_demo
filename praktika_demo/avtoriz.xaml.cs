5df91d05569550f81436bd8b361ef4bd93584c9b
kaoru <nsaidov530@gmail.com>
2024-06-08 17:45:21 +0300

    Добавьте файлы проекта.

---
+++ praktika_demo/avtoriz.xaml.cs
@@ -0,0 +1,113 @@
+﻿
+using System.Linq;
+using System.Windows;
+
+
+namespace praktika_demo
+{
+    public partial class avtoriz : Window
+    {
+        private praktika_vesnaEntities4 db;
+        int user_id;
+        public avtoriz()
+        {
+            InitializeComponent();
+            db = new praktika_vesnaEntities4();
+        }
+
+        private void Registr(object sender, RoutedEventArgs e)
+        {
+            Registrasia registrasia = new Registrasia();
+            registrasia.Show();
+            this.Close();
+        }
+        private bool ValidateInput()
+        {
+            string login = loginText.Text;
+            string password = passwordText.Password;
+
+            using (praktika_vesnaEntities4 db = new praktika_vesnaEntities4())
+            {
+                var userAuth = db.auth.FirstOrDefault(a => a.login == login && a.password == password);
+
+                if (userAuth == null)
+                {
+                    MessageBox.Show("Неверный логин или пароль.");
+                    return false;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(login))
+            {
+                MessageBox.Show("Пожалуйста, введите логин.");
+                return false;
+            }
+
+            if (string.IsNullOrWhiteSpace(password))
+            {
+                MessageBox.Show("Пожалуйста, введите пароль.");
+                return false;
+            }
+
+            return true;
+        }
+
+        private void vhod(object sender, RoutedEventArgs e)
+        {
+            if (!ValidateInput())
+            {
+                return;
+            }
+            string login = loginText.Text;
+            string password = passwordText.Password;
+
+            using (praktika_vesnaEntities4 db = new praktika_vesnaEntities4())
+            {
+                var userAuth = db.auth.FirstOrDefault(a => a.login == login && a.password == password);
+
+                if (userAuth == null)
+                {
+                    MessageBox.Show("Неверный логин или пароль.");
+                    return;
+                }
+
+                var user = userAuth.user;
+                user_id = user.userID; 
+
+                RedirectUserBasedOnRole(user.type_id.GetValueOrDefault());
+            }
+        }
+
+
+        private void RedirectUserBasedOnRole(int typeId)
+        {
+            Window targetWindow = null;
+
+            switch (typeId)
+            {
+                case 1: 
+                    targetWindow = new oper(user_id);
+                    break;
+                case 2: 
+                    targetWindow = new Master(user_id);
+                    break;
+                case 3: 
+                    targetWindow = new oper(user_id);
+                    break;
+                case 4: 
+                    targetWindow = new zakashik(user_id);
+                    break;
+                default:
+                    MessageBox.Show("Неизвестная роль.");
+                    return;
+            }
+
+            if (targetWindow != null)
+            {
+                targetWindow.Show();
+                this.Close();
+            }
+        }
+
+    }
+}
---
+++ praktika_demo/sozdat_zayvku.xaml.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace praktika_demo
+{
+    public partial class sozdat_zayvku : Window
+    {
+        private praktika_vesnaEntities4 db;
+        int requestiD; 
+        int user_id;
+
+        public sozdat_zayvku(int user_id)
+        {
+            InitializeComponent();
+            this.user_id = user_id;
+            db = new praktika_vesnaEntities4(); 
+        }
+        public void sozdat_zayavka()
+        {
+            DateTime? startDate = datePicker.SelectedDate;
+
+            string homeTechType = ((ComboBoxItem)techComboBox.SelectedItem).Content.ToString();
+
+            
+            string homeTechModel = modelTextBox.Text;
+
+            
+            string problemDescriptionText = problemDescription.Text;
+
+            
+            string requestStatus = ((ComboBoxItem)statusComboBox.SelectedItem).Content.ToString();
+
+            var newHomeTechType = new homeTechType
+            {
+                homeTechType1 = homeTechType,
+                homeTechModel = homeTechModel
+            };
+
+            var newStatus = new status
+            {
+                statusDescription = requestStatus
+            };
+
+           
+            var newRequest = new request
+            {
+                problemDescription = problemDescriptionText,
+                startDate = startDate,
+            };
+
+            // Проверяем, существует ли пользователь
+            var user = db.user.SingleOrDefault(u => u.userID == this.user_id);
+            if (user == null)
+            {
+                MessageBox.Show("Пользователь не найден.");
+                return;
+            }
+
+           
+            var requestClient = new request_client
+            {
+                clientID = this.user_id 
+            };
+
+            newRequest.homeTechType.Add(newHomeTechType);
+            newRequest.status.Add(newStatus);
+            newRequest.request_client.Add(requestClient);
+
+            
+            db.request.Add(newRequest);
+            db.SaveChanges();
+
+            
+            MessageBox.Show("Заявка успешно создана!");
+        }
+        private void sozdat(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                sozdat_zayavka();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка при создании заявки: " + ex.Message);
+            }
+        }
+
+        private void redactirovat(object sender, RoutedEventArgs e)
+        {
+            redaktirovat_zayvki redaktirovat_Zayvki = new redaktirovat_zayvki(user_id);
+            redaktirovat_Zayvki.Show();
+            this.Close();
+        }
+
+        private void problemTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void sozdat_z(object sender, RoutedEventArgs e)
+        {
+           
+        }
+
+        private void zayavki(object sender, RoutedEventArgs e)
+        {
+            zakashik zakashik = new zakashik(user_id);
+            zakashik.Show();
+            this.Close();
+        }
+
+        private void exist(object sender, RoutedEventArgs e)
+        {
+
+            this.Close();
+        }
+    }
+}
